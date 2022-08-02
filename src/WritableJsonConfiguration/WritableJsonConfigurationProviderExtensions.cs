ffc6511390a720747951c6a0c90bdaff05e37f0e
Kibnet Philosoff <kibnet@hotmail.com>
2022-08-02 14:19:48 +0300

    feat: New extension methods Get and Set

--- src/WritableJsonConfiguration/WritableJsonConfigurationProviderExtensions.cs
+++ src/WritableJsonConfiguration/WritableJsonConfigurationProviderExtensions.cs
@@ -6,7 +6,13 @@ using WritableJsonConfiguration;
 namespace Microsoft.Extensions.Configuration
 {
     public static class WritableJsonConfigurationProviderExtensions
-    {
+    {        
+        /// <summary>
+        /// Set value for current section
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static void Set(this IConfiguration configuration, object value)
         {
             switch (configuration)
@@ -28,6 +34,29 @@ namespace Microsoft.Extensions.Configuration
                 default:
                     throw new ArgumentOutOfRangeException(nameof(configuration));
             }
+        }
+
+        /// <summary>
+        /// Get section and set value
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="section"></param>
+        /// <param name="value"></param>
+        public static void Set(this IConfiguration configuration, string section, object value)
+        {
+            configuration.GetSection(section).Set(value);
+        }
+        
+        /// <summary>
+        /// Get object by section
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="configuration"></param>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public static T Get<T>(this IConfiguration configuration, string section)
+        {
+            return configuration.GetSection(section).Get<T>();
         }
     }
 }
